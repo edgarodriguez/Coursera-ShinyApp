ec613b433fe6811b19c7539f66c50ef7dd6eb628
edgarodriguez <edgarodriguez@outlook.com>
2020-09-03 18:26:04 -0500

    Upload Shiny project

---
+++ Course-Project-Data-Products/server.R
@@ -0,0 +1,48 @@
+library(shiny)
+library(drc)
+data <- read.csv("LULC-BioclimaticParameters.csv", encoding = "UTF-8")
+data2<-data
+
+shinyServer(function(input,output){
+    
+    model1 <- lm(ETa.HAM ~ P,data=data2)
+    model2 <- drm(ETa.HAR ~ P, data=data2, fct=MM.2() )
+
+    model1pred <- reactive({
+        PInput <- input$sliderP
+        predict(model1, newdata=data.frame(P = PInput))
+    })
+    
+    model2pred <- reactive({
+        PInput <- input$sliderP
+        predict(model2, newdata=
+                    data.frame(P=PInput))
+    })
+    output$plot1 <- renderPlot({
+        PInput <- input$sliderP
+        
+        plot(data2$P,data2$ETa.AVG, xlab="Precipitation (mm)",
+             ylab="Actual Evapotranspiration (mm)",bty="n",pch=16,xlim=c(10,2000),ylim=c(10,2000),
+             col=rgb(red=0.2, green=0.2, blue=1.0, alpha=0.05))
+        if(input$showModel1){
+            abline(model1, col="red",lwd=2)
+        }
+        if(input$showModel2){
+            model2lines <- predict(model2,newdata = data.frame(P=10:2000))
+            lines(10:2000,model2lines, col="green",lwd=2)
+        
+        }
+        legend(25,250,c("Model 1 Prediction","Model 2 Prediction"),pch=16,
+               col= c("red","green"),bty="n",cex=1.2)
+        points(PInput,model1pred(),col="red",pch=16,cex=2)
+        points(PInput,model2pred(),col="green",pch=16,cex=2)
+    })
+    
+    output$pred1 <- renderText({
+        model1pred()
+    })
+    output$pred2 <- renderText({
+        model2pred()
+    })
+    })
+
---
+++ ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+shinyUI(fluidPage(
+    titlePanel("Data Products: Predict Evapotranspiration with Precipitation data in Yucatán Península, México"),
+    sidebarLayout(
+        sidebarPanel(
+        sliderInput("sliderP", "What is the Precipitation?",10,2000, value=1100),
+        checkboxInput("showModel1","show/hide Model1", value=TRUE),
+        checkboxInput("showModel2","show/hide Model2", value=TRUE),
+        ),                      
+        mainPanel(
+            plotOutput("plot1"),
+            h4("Model1: Predict AET (Hamon Method) with lineal regression (lm function):"),
+            textOutput("pred1"),
+            h4("Model2: Predict AET (Hargreaves Method) with logistic regression (drc MM.2 funtion):"),
+            textOutput("pred2")
+        )
+    )
+))
